4f97331246cb5ab3ccafa07782253a57a32bd66d
KrisKipper <yarevyakinakristina@gmail.com>
2023-06-15 15:24:42 +0300

    Initial

---
+++ README.md
@@ -0,0 +1,31 @@
+# Markup Boilerplate
+
+**Installation**
+
+```
+yarn
+```
+
+**Start Dev Server**
+
+```
+yarn serve
+```
+
+**Build Prod Version**
+
+```
+yarn prod
+```
+
+**Build Dev Version**
+
+```
+yarn dev
+```
+
+**Run Watcher**
+
+```
+yarn watch
+```
---
+++ webpack.config.js
@@ -0,0 +1,92 @@
+const path = require('path')
+const fs = require('fs')
+
+const webpack = require('webpack')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const HTMLWebpackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+
+const dotenv = require('dotenv').config()
+
+const pagesDir = path.resolve(__dirname, 'src/templates/pages')
+const pages = fs.readdirSync(pagesDir).filter(fileName => fileName.endsWith('.pug'))
+
+module.exports = {
+  context: path.resolve(__dirname, 'src'),
+  entry: {
+    app: ['@babel/polyfill', './app.js']
+  },
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'postcss-loader',
+          'sass-loader'
+        ],
+      },
+      {
+        test: /\.pug$/,
+        loader: 'pug-loader',
+        options: {
+          self: true
+        },
+      },
+      {
+        test: /\.(ico|jpg|jpeg|png|gif|eot|otf|webp|ttf|woff|woff2|svg|mp4)$/,
+        use: {
+          loader: 'file-loader',
+          options: {
+            esModule: false,
+            name: '[path][name].[ext]'
+          }
+        }
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: [
+                '@babel/preset-env'
+              ]
+            }
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new webpack.DefinePlugin({
+      'process.env': JSON.stringify(dotenv.parsed)
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].css'
+    }),
+    ...pages.map(page => {
+      return new HTMLWebpackPlugin({
+        template: `${pagesDir}/${page}`,
+        filename: `./${page.replace(/\.pug/, '.html')}`
+      })
+    })
+  ],
+  devServer: {
+    host: process.env.DEV_SERVER_HOST,
+    port: process.env.DEV_SERVER_PORT
+  },
+  devtool: 'source-map'
+}
